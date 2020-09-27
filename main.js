556d7ca83e1717ce2c856b35a2f7b497a707a698
Mikhail Nepomnyashiy <mnepomnyashiy@specialist.ru>
2020-09-27 16:09:56 +0300

    lab2 files was uploaded

--- main.js
+++ main.js
@@ -1,41 +1,23 @@
-// Получить ссылку на список карточек .cards
-const cardList = document.querySelector('.cards');
-// Переменная для получения кнопки
-let loadMoreBtn = null;
-// Вызов функции-инициализации
-initLoadBtn();
+// получаем ссылку на форму
+const form;
+// получаем ссылку на список дел ul
+const todos;
 
-// Написать функцию, которая
-// 1) получает ссылку на кнопку в переменную loadMoreBtn
-// 2) Проверяет наличие кнопки на страницы
-// 3) В случае наличия кнопки добавляет на нее обработчик клика (вызов функции loadMoreHandler)
-function initLoadBtn() {
-    loadMoreBtn = document.querySelector('button[data-more]');
+// загрузка задач из фейковой БД
+loadTasks();
 
-    if (loadMoreBtn != null) {
-        loadMoreBtn.addEventListener('click', loadMoreHandler)
-    }
-}
+// добавление "листнера" на отправку формы
+form.addEventListener('submit', submitHandler);
 
-// Написать функцию, которая
-// 1) получает ссылку для запроса из дата-атрибута кнопки 'data-more'
-// 2) делает fetch запрос по ссылке
-// 3) по получению данных:
-// 3.1) с кнопки "Загрузить еще" снимается обработчик события
-// 3.2) текущая кнопка "Загрузить еще" удаляется из DOM
-// 3.3) к списку карточек добавляется полученный набор HTML-текста
-// 3.3) вызывается функция initLoadBtn для обработки новой кнопки "Загрузить еще"
-// 4) если данные не получены, должен появляться предупреждающий alert
-function loadMoreHandler() {
-    const url = this.dataset.more; // this.getAttributes('data-more')
+// обработка отправки формы с заглушкой на время отсутствия бэка
+function submitHandler() {}
 
-    fetch(url)
-        .then(response => response.text())
-        .then(text => {
-            loadMoreBtn.removeEventListener('click', loadMoreHandler);
-            loadMoreBtn.remove();
-            cardList.insertAdjacentHTML('beforeend', text);
-            initLoadBtn();
-        })
-        .catch(err => alert(err))
-}
+// загрузка задач из фейковой БД (localStorage)
+function loadTasks() {}
+
+// имитация запроса к серверу и получение ответа от него
+// пишем и комментируем fetch, реализуем загрушку через Promise
+function fakePostTask(task) {}
+
+// создание разметки для конкретной задачи
+function createTaskHtml(task) {}
